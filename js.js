782be6bffcf14f2e3fabd26ac0c18267f95c78b0
RonJie123 <154306490+RonJie123@users.noreply.github.com>
2023-12-19 16:51:12 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function login(){
+    window.location.href = "http://localhost/project/login.html";
+}
+function goregister(){
+    window.location.href = "http://127.0.0.1:5500/htdocs/final%20project/register.html";
+}
+function click(){
+    window.location.href = "http://127.0.0.1:5500/blacklabel.html";
+}
+function logout(){
+    window.location.href = "http://127.0.0.1:5500/html.html";
+}
